9ac232952053ca4cde44a426030801900cb48a28
Awab Masroor <53250798+secretstf@users.noreply.github.com>
2024-09-07 20:14:11 -0400

    intial start of next.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to My First Next.js Web Page!</h1>
+        <p>This is a simple Next.js app.</p>
+      </div>
+    )
+  }
+  
