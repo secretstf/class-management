60c250345870260e741a35542f1ac52d1e0a466e
Awab Masroor <53250798+secretstf@users.noreply.github.com>
2024-09-12 00:30:37 -0400

    add delete to document api

--- pages/api/firebase/document.js
+++ pages/api/firebase/document.js
@@ -54,7 +54,28 @@ export default async function handler(req, res) {
       data = await JSON.parse(data);
       await setDoc(doc(firestore, collection_name, doc_id), data);
 
-    } else {
+    } else if (req.method == "DELETE") {
+      // check if id is in the query
+      if (!("id" in req.query)) {
+        res.status(400).json({ error: "Missing document id in query" });
+      } else if (!("collection" in req.query)) {
+        res.status(400).json({ error: "Missing collection name in query" });
+      }
+
+      // get the user with the id
+      const id = req.query.id;
+      const collection_name = req.query.collection;
+
+      const snapshot = doc(firestore, collection_name, id);
+      const docRef = await getDoc(snapshot);
+
+      if (docRef.exists()) {
+        await snapshot.delete();
+        res.status(200).json({ message: "Document deleted" });
+      } else {
+        res.status(404).json({ error: "Document does not exist!" });
+      }
+    }else {
       res.status(405).json({ error: "Invalid request method" });
     }
   } catch (error) {
